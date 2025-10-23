a4bebfcaeebc170f1689a080b89ba4047d88ee8e
hellart1 <ashirnin9@gmail.com>
2025-10-23 20:06:35 +0700

    added s3mixins

--- pastebin/paste/utils.py
+++ pastebin/paste/utils.py
@@ -1,16 +1,73 @@
+import os
+
+import boto3
+from botocore.exceptions import ClientError
+from django.conf import settings
 from django.views.generic.detail import SingleObjectMixin
 
 
-class StrObjectMixin(SingleObjectMixin):
-    str_field = 'data'
+class S3ConnectMixin:
+    def s3_client(self):
+        return boto3.client(
+            's3',
+            endpoint_url=os.getenv("AWS_S3_ENDPOINT_URL"),
+            region_name=os.getenv("ru-central1"),
+            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+        )
 
-    def get_object(self, queryset=None):
-        if queryset is None:
-            queryset = self.get_queryset()
+    def s3_resource(self):
+        return boto3.resource(
+            's3',
+            endpoint_url=os.getenv("AWS_S3_ENDPOINT_URL"),
+            region_name=os.getenv("ru-central1"),
+            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+        )
 
-        str = self.kwargs.get(self.str_field)
 
-        if not str:
-            raise AttributeError("Str is required to get an object")
+class S3UtilsMixin(S3ConnectMixin):
 
-        return queryset.filter(**{self.str_field: str}).get()
+    def put_object_in_s3(self, file_hash, text, resource=None, bucket_name=settings.AWS_STORAGE_BUCKET_NAME):
+        resource = resource or self.s3_resource()
+        bucket = resource.Bucket(bucket_name)
+        try:
+            return bucket.put_object(
+                Key=f"{file_hash}.txt",
+                Body=text
+            )
+        except Exception as e:
+            print(f"Ошибка: {e}")
+            return None
+
+    def create_presigned_url(
+            self, object_name, client=None, bucket_name=settings.AWS_STORAGE_BUCKET_NAME, expiration=None
+    ):
+        client = client or self.s3_client()
+        print(client)
+        print(object_name)
+        try:
+            return client.generate_presigned_url(
+                ClientMethod='get_object',
+                Params={'Bucket': bucket_name, 'Key': f"{object_name}.txt"},
+                ExpiresIn=expiration
+            )
+
+        except ClientError as e:
+            print("ошибка клиента")
+            # logging.error(e)
+            return None
+
+# class StrObjectMixin(SingleObjectMixin):
+#     str_field = 'data'
+#
+#     def get_object(self, queryset=None):
+#         if queryset is None:
+#             queryset = self.get_queryset()
+#
+#         str = self.kwargs.get(self.str_field)
+#
+#         if not str:
+#             raise AttributeError("Str is required to get an object")
+#
+#         return queryset.filter(**{self.str_field: str}).get()
--- pastebin/paste/views.py
+++ pastebin/paste/views.py
@@ -19,10 +19,10 @@ from .forms import TextForm
 from .models import Paste
 from .s3_utils import get_unique_hash, s3_resource, s3_client
 from .serializers import PasteSerializer
-from .utils import StrObjectMixin
+from .utils import *
 
 
-class Home(FormView):
+class Home(S3UtilsMixin, FormView):
     form_class = TextForm
     template_name = "paste/home.html"
     model = Paste
@@ -30,26 +30,6 @@ class Home(FormView):
 
     def get_success_url(self):
         return reverse_lazy('user_text', kwargs={'data': self.paste_hash})
-
-    # def create_presigned_url(self, client, bucket_name, object_name, expiration):
-    #
-    #     try:
-    #         return client.generate_presigned_url(
-    #             ClientMethod='get_object',
-    #             Params={'Bucket': bucket_name, 'Key': object_name},
-    #             ExpiresIn=expiration
-    #         )
-    #
-    #     except ClientError as e:
-    #         # logging.error(e)
-    #         return None
-
-    def put_object_in_s3(self, resource, bucket_name, file_hash, text):
-        bucket = resource.Bucket(bucket_name)
-        return bucket.put_object(
-            Key=f"{file_hash}.txt",
-            Body=text
-        )
 
     def form_valid(self, form):
         print('form_valid')
@@ -61,8 +41,6 @@ class Home(FormView):
         print(self.paste_hash, lifespan)
 
         self.put_object_in_s3(
-            resource=s3_resource(),
-            bucket_name=settings.AWS_STORAGE_BUCKET_NAME,
             file_hash=self.paste_hash,
             text=paste_text
         )
@@ -72,86 +50,81 @@ class Home(FormView):
         print('создался обьект в бд')
         return super().form_valid(form)
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['post'] = self.download_from_s3(key=)
-
 
 class User_text(DetailView):
     model = Paste
     template_name = "paste/user_text.html"
     context_object_name = 'post'
 
-    # def get_object_from_s3(self, resource, bucket_name, s3_key):
-    #     bucket = resource.Bucket(bucket_name)
-    #     obj = bucket.Object(f"{s3_key}.txt")
-    #     return obj.get()['Body'].read().decode('utf-8')
-
     def get_object(self, queryset=None):
         endpoint = reverse('paste_api', kwargs={'hash': self.kwargs.get('data')})
-        response = requests.get(
-            self.request.build_absolute_uri(endpoint)
-        ).json()
+        print(endpoint)
+        try:
+            response = requests.get(
+                self.request.build_absolute_uri(endpoint)
+            ).json()
+            print(response)
 
-        if 'download_url' not in response:
-            print('Нет download_url в json ответе')
-            raise Http404
-        else:
-            content = requests.get(response['download_url'])
-            if content.status_code == 200:
-                return content.text
-            else:
+            if 'download_url' not in response:
+                print('Нет download_url в json ответе')
                 raise Http404
+            else:
+                content = requests.get(response['download_url'])
+                if content.status_code == 200:
+                    return content.text
+                else:
+                    raise Http404
+        except Exception as e:
+            print(f'Ошибка: {e}')
 
 
-class PasteAPIList(generics.RetrieveAPIView):
+class PasteAPIList(S3UtilsMixin, generics.RetrieveAPIView):
     queryset = Paste.objects.all()
     serializer_class = PasteSerializer
     lookup_field = 'hash'
 
-    def create_presigned_url(self, client, bucket_name, object_name, expiration):
-        try:
-            return client.generate_presigned_url(
-                ClientMethod='get_object',
-                Params={'Bucket': bucket_name, 'Key': object_name},
-                ExpiresIn=expiration
-            )
-
-        except ClientError as e:
-            print("ошибка клиента")
-            # logging.error(e)
-            return None
-
-    def check_expired(self, obj):
+    def get_expiration_seconds(self, obj):
         expiration_delta = {
-            'N': 'never',
-            'B': 'burn_after_read',
             '10M': timedelta(minutes=10),
             '1H': timedelta(hours=1),
             '1D': timedelta(days=1),
         }
         expiration = expiration_delta[obj.expiration_type]
-        print(type(obj.created_at))
-        print(type(datetime.now()))
-        # created_at = datetime.strptime(obj.created_at, "%Y-%m-%d %H:%M:%S")
+
+        created_time = obj.created_at.timestamp()
+        time_now = datetime.now().timestamp()
+
+        return created_time + expiration.total_seconds() - time_now
+
+    def check_expired(self, obj):
+        expiration_delta = {
+            '10M': timedelta(minutes=10),
+            '1H': timedelta(hours=1),
+            '1D': timedelta(days=1),
+        }
+        if obj.expiration_type == 'N':
+            return None
+        elif obj.expiration_type == 'B':
+            return None
+        expiration = expiration_delta[obj.expiration_type]
 
         # если отправить запрос за 5 секунд до конца действия ссылки, она еще будет действовать 10 минут
-        return datetime.now().timestamp() < (obj.created_at + expiration).timestamp()
+        # идея: подключить redis для кэширования ссылки вместо постоянного запроса новой
+        time_now = datetime.now().timestamp()
+        created_time = (obj.created_at + expiration).timestamp()
+        if time_now < created_time:
+            return self.get_expiration_seconds(obj)
 
     def retrieve(self, request, *args, **kwargs):
         obj = self.get_object()
-        # если срок действия истек, raise 404
-        # иначе создаем ссылку
-        if self.check_expired(obj):
-            presigned_url = self.create_presigned_url(
-                client=s3_client(),
-                bucket_name=settings.AWS_STORAGE_BUCKET_NAME,
-                object_name=f"{self.kwargs.get("hash")}.txt",
-                expiration=600
-            )
+        expiration_time = self.check_expired(obj)
 
+        if obj.expiration_type == 'N':
+            presigned_url = self.create_presigned_url(obj.hash)
+        elif expiration_time:
+            presigned_url = self.create_presigned_url(obj.hash, expiration_time)
         else:
-            print('Срок действия истек')
+            print('Срок действия пасты истек')
             return Response(status=404)
 
         serializer = self.get_serializer_class()(obj, context={'download_url': presigned_url})
